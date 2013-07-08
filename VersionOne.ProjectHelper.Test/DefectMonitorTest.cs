cb131dc88cd308446d7eaa0d3325f72ebeac25c6
Ian Buchanan <ian.buchanan@versionone.com>
2013-07-08 17:21:47 -0400

    adding behavior to monitor new defects

---
+++ VersionOne.ProjectHelper.Test/DefectMonitorTest.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VersionOne.SDK.APIClient;
+using VersionOne.SDK.ObjectModel;
+
+namespace VersionOne.ProjectHelper.Test
+{
+    [TestClass]
+    public class DefectMonitorTest
+    {
+        [TestMethod]
+        public void TestGetMostRecentChangedDateTime()
+        {
+            // Given a connection to an instance of VersionOne at https://www14.v1host.com/v1sdktesting/ with user credentials for admin
+            var instance = new V1Instance("https://www14.v1host.com/v1sdktesting/", "admin", "admin");
+            // And a new schedule with 7 day length and no gap
+            var schedule = instance.Create.Schedule(Name("Schedule"), Duration.Parse(@"7 Days"), Duration.Parse(@"0 Days"));
+            // And a new parent project called "Target Project 1" under the root with that schedule
+            var rootProject = instance.Get.ProjectByID(AssetID.FromToken("Scope:0"));
+            var targetProject = instance.Create.Project(Name("Target Project"), rootProject, DateTime.Now, schedule);
+            // And a new defect under "Target Project"
+            var newDefect = instance.Create.Defect(Name("New Defect"), targetProject);
+            // And a new project helper
+            var v1System = new SystemAllProjects(instance.ApiClient.Services, instance.ApiClient.MetaModel);
+            // And the list of all projects
+            var projectList = v1System.GetProjects();
+            // And a new defect monitor
+            var monitor = new DefectMonitor(instance.ApiClient.Services, instance.ApiClient.MetaModel);
+            // And the parent project is registered with the monitor
+            monitor.MonitoredProjects.Add(projectList[targetProject.ID.Token]);
+            // When I check to see the most recent change date
+            var actual = monitor.GetMostRecentChangeDateTime();
+            // Then the monitor tells me the date from the new defect.
+            var expected = newDefect.ChangeDate.ToUniversalTime();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestHasChangedSinceWithNoChange()
+        {
+            // Given a connection to an instance of VersionOne at https://www14.v1host.com/v1sdktesting/ with user credentials for admin
+            var instance = new V1Instance("https://www14.v1host.com/v1sdktesting/", "admin", "admin");
+            // And a new schedule with 7 day length and no gap
+            var schedule = instance.Create.Schedule(Name("Schedule"), Duration.Parse(@"7 Days"), Duration.Parse(@"0 Days"));
+            // And a new parent project called "Target Project 1" under the root with that schedule
+            var rootProject = instance.Get.ProjectByID(AssetID.FromToken("Scope:0"));
+            var targetProject = instance.Create.Project(Name("Target Project"), rootProject, DateTime.Now, schedule);
+            // And a defect under "Target Project"
+            var newDefect = instance.Create.Defect(Name("New Defect"), targetProject);
+            // And a new project helper
+            var v1System = new SystemAllProjects(instance.ApiClient.Services, instance.ApiClient.MetaModel);
+            // And the list of all projects
+            var projectList = v1System.GetProjects();
+            // And a new defect monitor
+            var monitor = new DefectMonitor(instance.ApiClient.Services, instance.ApiClient.MetaModel);
+            // And the parent project is registered with the monitor
+            monitor.MonitoredProjects.Add(projectList[targetProject.ID.Token]);
+            // And the most recent change date is for the defect
+            var lastChange = monitor.GetMostRecentChangeDateTime();
+            // When check whether the system has changed since the remembered change date
+            var actual = monitor.HasChangedSince(lastChange);
+            // Then the monitor tells me there is no change
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void TestHasChangedSinceWithAChange()
+        {
+            // Given a connection to an instance of VersionOne at https://www14.v1host.com/v1sdktesting/ with user credentials for admin
+            var instance = new V1Instance("https://www14.v1host.com/v1sdktesting/", "admin", "admin");
+            // And a new schedule with 7 day length and no gap
+            var schedule = instance.Create.Schedule(Name("Schedule"), Duration.Parse(@"7 Days"), Duration.Parse(@"0 Days"));
+            // And a new parent project called "Target Project 1" under the root with that schedule
+            var rootProject = instance.Get.ProjectByID(AssetID.FromToken("Scope:0"));
+            var targetProject = instance.Create.Project(Name("Target Project"), rootProject, DateTime.Now, schedule);
+            // And a defect under "Target Project"
+            var newDefect = instance.Create.Defect(Name("New Defect"), targetProject);
+            // And a new project helper
+            var v1System = new SystemAllProjects(instance.ApiClient.Services, instance.ApiClient.MetaModel);
+            // And the list of all projects
+            var projectList = v1System.GetProjects();
+            // And a new defect monitor
+            var monitor = new DefectMonitor(instance.ApiClient.Services, instance.ApiClient.MetaModel);
+            // And the parent project is registered with the monitor
+            monitor.MonitoredProjects.Add(projectList[targetProject.ID.Token]);
+            // And the most recent change date is for the defect
+            var lastChange = monitor.GetMostRecentChangeDateTime();
+            // When I create a new defect
+            var newerDefect = instance.Create.Defect(Name("Newer Defect"), targetProject);
+            // And check whether the system has changed since the remembered change date
+            var actual = monitor.HasChangedSince(lastChange);
+            // Then the monitor tells me there is a change
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void TestGetMostRecentNewDateTime()
+        {
+            // Given a connection to an instance of VersionOne at https://www14.v1host.com/v1sdktesting/ with user credentials for admin
+            var instance = new V1Instance("https://www14.v1host.com/v1sdktesting/", "admin", "admin");
+            // And a new schedule with 7 day length and no gap
+            var schedule = instance.Create.Schedule(Name("Schedule"), Duration.Parse(@"7 Days"), Duration.Parse(@"0 Days"));
+            // And a new parent project called "Target Project 1" under the root with that schedule
+            var rootProject = instance.Get.ProjectByID(AssetID.FromToken("Scope:0"));
+            var targetProject = instance.Create.Project(Name("Target Project"), rootProject, DateTime.Now, schedule);
+            // And a new defect under "Target Project"
+            var newDefect = instance.Create.Defect(Name("New Defect"), targetProject);
+            // And a new project helper
+            var v1System = new SystemAllProjects(instance.ApiClient.Services, instance.ApiClient.MetaModel);
+            // And the list of all projects
+            var projectList = v1System.GetProjects();
+            // And a new defect monitor
+            var monitor = new DefectMonitor(instance.ApiClient.Services, instance.ApiClient.MetaModel);
+            // And the parent project is registered with the monitor
+            monitor.MonitoredProjects.Add(projectList[targetProject.ID.Token]);
+            // When I check to see the most recent new date
+            var actual = monitor.GetMostRecentNewDateTime();
+            // Then the monitor tells me the date from the new defect.
+            var expected = newDefect.CreateDate;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestHasNewSinceWithNoChange()
+        {
+            // Given a connection to an instance of VersionOne at https://www14.v1host.com/v1sdktesting/ with user credentials for admin
+            var instance = new V1Instance("https://www14.v1host.com/v1sdktesting/", "admin", "admin");
+            // And a new schedule with 7 day length and no gap
+            var schedule = instance.Create.Schedule(Name("Schedule"), Duration.Parse(@"7 Days"), Duration.Parse(@"0 Days"));
+            // And a new parent project called "Target Project 1" under the root with that schedule
+            var rootProject = instance.Get.ProjectByID(AssetID.FromToken("Scope:0"));
+            var targetProject = instance.Create.Project(Name("Target Project"), rootProject, DateTime.Now, schedule);
+            // And a defect under "Target Project"
+            var newDefect = instance.Create.Defect(Name("New Defect"), targetProject);
+            // And a new project helper
+            var v1System = new SystemAllProjects(instance.ApiClient.Services, instance.ApiClient.MetaModel);
+            // And the list of all projects
+            var projectList = v1System.GetProjects();
+            // And a new defect monitor
+            var monitor = new DefectMonitor(instance.ApiClient.Services, instance.ApiClient.MetaModel);
+            // And the parent project is registered with the monitor
+            monitor.MonitoredProjects.Add(projectList[targetProject.ID.Token]);
+            // And the most recent new date is for the defect
+            var lastNew = monitor.GetMostRecentNewDateTime();
+            // When I check whether the system has changed since the remembered new date
+            var actual = monitor.HasNewSince(lastNew);
+            // Then the monitor tells me there is nothing new
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void TestHasNewSinceWithAChange()
+        {
+            // Given a connection to an instance of VersionOne at https://www14.v1host.com/v1sdktesting/ with user credentials for admin
+            var instance = new V1Instance("https://www14.v1host.com/v1sdktesting/", "admin", "admin");
+            // And a new schedule with 7 day length and no gap
+            var schedule = instance.Create.Schedule(Name("Schedule"), Duration.Parse(@"7 Days"), Duration.Parse(@"0 Days"));
+            // And a new parent project called "Target Project 1" under the root with that schedule
+            var rootProject = instance.Get.ProjectByID(AssetID.FromToken("Scope:0"));
+            var targetProject = instance.Create.Project(Name("Target Project"), rootProject, DateTime.Now, schedule);
+            // And a defect under "Target Project"
+            var newDefect = instance.Create.Defect(Name("New Defect"), targetProject);
+            // And a new project helper
+            var v1System = new SystemAllProjects(instance.ApiClient.Services, instance.ApiClient.MetaModel);
+            // And the list of all projects
+            var projectList = v1System.GetProjects();
+            // And a new defect monitor
+            var monitor = new DefectMonitor(instance.ApiClient.Services, instance.ApiClient.MetaModel);
+            // And the parent project is registered with the monitor
+            monitor.MonitoredProjects.Add(projectList[targetProject.ID.Token]);
+            // And the most recent new date is for the defect
+            var lastNew = monitor.GetMostRecentNewDateTime();
+            // When I create a new defect
+            var newerDefect = instance.Create.Defect(Name("Newer Defect"), targetProject);
+            // And check whether the system has new since the remembered change date
+            var actual = monitor.HasChangedSince(lastNew);
+            // Then the monitor tells me there is a new defect
+            Assert.IsTrue(actual);
+        }
+
+        private static string Name(string prefix)
+        {
+            return (prefix + " SystemAllProjects Helper " + DateTime.Now);
+        }
+
+    }
+}
--- VersionOne.ProjectHelper/DefectMonitor.cs
+++ VersionOne.ProjectHelper/DefectMonitor.cs
@@ -6,15 +6,16 @@ using VersionOne.SDK.APIClient;
 
 namespace VersionOne.ProjectHelper
 {
-    public class DefectChangeMonitor
+    public class DefectMonitor
     {
         private readonly IServices _services;
         private readonly IMetaModel _meta;
         private DateTime? _mostRecentChangeDateTime;
+        private DateTime? _mostRecentNewDateTime;
 
         public IList<Project> MonitoredProjects { get; set; }
 
-        public DefectChangeMonitor(IServices services, IMetaModel meta)
+        public DefectMonitor(IServices services, IMetaModel meta)
         {
             _services = services;
             _meta = meta;
@@ -74,6 +75,62 @@ namespace VersionOne.ProjectHelper
                 }
             }
             return _mostRecentChangeDateTime;
+        }
+
+        public DateTime? GetMostRecentNewDateTime()
+        {
+            var defectType = _meta.GetAssetType("Defect");
+            var query = new Query(defectType);
+            var projectAttribute = defectType.GetAttributeDefinition("Scope");
+            var projectTerm = new FilterTerm(projectAttribute);
+            projectTerm.Equal(MonitoredProjects.Select(project => project.ScopeId).Cast<Object>().ToArray());
+
+            var createAttribute = defectType.GetAttributeDefinition("CreateDate");
+            query.Selection.Add(createAttribute);
+            if (_mostRecentNewDateTime.HasValue)
+            {
+                var createTerm = new FilterTerm(createAttribute);
+                createTerm.Greater(_mostRecentNewDateTime.Value.ToString("yyyy-MM-ddTHH:mm:ss.fff", CultureInfo.InvariantCulture));
+                query.Filter = new AndFilterTerm(createTerm, projectTerm);
+            }
+            else
+            {
+                query.Filter = projectTerm;
+            }
+
+            var result = _services.Retrieve(query);
+
+            if (result.TotalAvaliable > 0)
+            {
+                foreach (var asset in result.Assets)
+                {
+                    var projectNewChangeDate = DB.DateTime(asset.GetAttribute(createAttribute).Value);
+                    if ((!_mostRecentNewDateTime.HasValue) || (projectNewChangeDate > _mostRecentNewDateTime))
+                    {
+                        _mostRecentNewDateTime = projectNewChangeDate;
+                    }
+                }
+            }
+            return _mostRecentNewDateTime;
+        }
+
+        public bool HasNewSince(DateTime? lastCheckedDateTime)
+        {
+            if (!lastCheckedDateTime.HasValue)
+            {
+                return true;
+            }
+            var defectType = _meta.GetAssetType("Defect");
+            var query = new Query(defectType);
+            var createAttribute = defectType.GetAttributeDefinition("CreateDate");
+            var createTerm = new FilterTerm(createAttribute);
+            createTerm.Greater(lastCheckedDateTime.Value.ToString("yyyy-MM-ddTHH:mm:ss.fff", CultureInfo.InvariantCulture));
+            var projectAttribute = defectType.GetAttributeDefinition("Scope");
+            var projectTerm = new FilterTerm(projectAttribute);
+            projectTerm.Equal(MonitoredProjects.Select(project => project.ScopeId).Cast<Object>().ToArray());
+            query.Filter = new AndFilterTerm(createTerm, projectTerm);
+            var result = _services.Retrieve(query);
+            return result.TotalAvaliable > 0;
         }
     }
 }
